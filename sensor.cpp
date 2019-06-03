851a37979a571f03a2a4a978c53e5edeceb7750b
Cameron Lee <cam.w.lee@gmail.com>
2019-06-02 21:14:40 -0600

    Attempting to improve sensor reading stability:
    * Performing an analogRead() without using the result before the real
    reads.
    * Averaging five samples.

--- sensor.cpp
+++ sensor.cpp
@@ -2,6 +2,13 @@
 
 #define FACTOR SENSOR_ADC_FACTOR
 
+#define THROW1AVG5(x) (float((x * 0) + x + x + x + x + x) / 5.0)
+    // Throw away the first reading and average the next five.
+    // This is to try and improve stability.
+
+#define VOLTAGE_READING(pin, factor) (THROW1AVG5(analogRead(pin)) * FACTOR * factor)
+#define CURRENT_READING(pin, zero, factor, offset) ((((THROW1AVG5(analogRead(pin)) * FACTOR) - zero) * factor) + offset)
+
 SensorManager::SensorManager() : voltage(), current(), energy(), nextReadTime(0), currentOffset() {}
 
 void SensorManager::setup(){
@@ -10,34 +17,28 @@ void SensorManager::setup(){
 void SensorManager::refresh(){
     if (millis() > nextReadTime){
         #ifdef LOAD_VOLTAGE_PIN
-            voltage[LOAD] = float(analogRead(LOAD_VOLTAGE_PIN)) * FACTOR * LOAD_VOLTAGE_FACTOR;
+            voltage[LOAD] = VOLTAGE_READING(LOAD_VOLTAGE_PIN, LOAD_CURRENT_FACTOR);
         #endif
         #ifdef LOAD_CURRENT_PIN
-            current[LOAD] = (((float(analogRead(LOAD_CURRENT_PIN)) * FACTOR)
-                - LOAD_CURRENT_ZERO) * LOAD_CURRENT_FACTOR) + currentOffset[LOAD];
+            current[LOAD] = CURRENT_READING(LOAD_CURRENT_PIN, LOAD_CURRENT_ZERO, LOAD_CURRENT_FACTOR, currentOffset[LOAD]);
         #endif
-        energy[LOAD] += voltage[LOAD] * current[LOAD]
-                * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
+        energy[LOAD] += voltage[LOAD] * current[LOAD] * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
 
         #ifdef PANEL_VOLTAGE_PIN
-            voltage[PANEL] = float(analogRead(PANEL_VOLTAGE_PIN)) * FACTOR * PANEL_VOLTAGE_FACTOR;
+            voltage[PANEL] =VOLTAGE_READING(PANEL_VOLTAGE_PIN, PANEL_VOLTAGE_FACTOR);
         #endif
         #ifdef PANEL_CURRENT_PIN
-            current[PANEL] = (((float(analogRead(PANEL_CURRENT_PIN)) * FACTOR)
-                - PANEL_CURRENT_ZERO) * PANEL_CURRENT_FACTOR) + currentOffset[PANEL];
+            current[PANEL] = CURRENT_READING(PANEL_CURRENT_PIN, PANEL_CURRENT_ZERO, PANEL_CURRENT_FACTOR, currentOffset[PANEL]);
         #endif
-        energy[PANEL] += voltage[PANEL] * current[PANEL]
-                * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
+        energy[PANEL] += voltage[PANEL] * current[PANEL] * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
 
         #ifdef LOAD2_VOLTAGE_PIN
-            voltage[LOAD2] = float(analogRead(LOAD2_VOLTAGE_PIN)) * FACTOR * LOAD2_VOLTAGE_FACTOR;
+            voltage[LOAD2] = VOLTAGE_READING(LOAD2_VOLTAGE_PIN, LOAD2_VOLTAGE_FACTOR);
         #endif
         #ifdef LOAD2_CURRENT_PIN
-            current[LOAD2] = (((float(analogRead(LOAD2_CURRENT_PIN)) * FACTOR)
-                - LOAD2_CURRENT_ZERO) * LOAD2_CURRENT_FACTOR) + currentOffset[LOAD2];
+            current[LOAD2] = CURRENT_READING(LOAD2_CURRENT_PIN, LOAD2_CURRENT_ZERO, LOAD2_CURRENT_FACTOR, currentOffset[LOAD2]);
         #endif
-        energy[LOAD2] += voltage[LOAD2] * current[LOAD2]
-                * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
+        energy[LOAD2] += voltage[LOAD2] * current[LOAD2] * (SENSOR_PERIOD_MILLIS + millis() - nextReadTime) / 1000.0;
 
 
         nextReadTime += SENSOR_PERIOD_MILLIS;
