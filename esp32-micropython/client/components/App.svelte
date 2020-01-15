f26d6ffbc00469a548ed16dc789e65178cc3c677
Cameron Lee <cam.w.lee@gmail.com>
2020-01-14 22:11:15 -0700

    WIP: More svelte. Styles and MaterialUI elements.

--- esp32-micropython/client/components/App.svelte
+++ esp32-micropython/client/components/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import DiskUsage from './DiskUsage.svelte';
+  import FullPageProgress from './FullPageProgress.svelte';
+  import "@material/layout-grid/mdc-layout-grid";
 
 
   let API_URL = process.env.API_URL || "http://" + location.host;
@@ -10,11 +12,11 @@
     return "time=" + time;
   }
 
+  let loading = true;
+  let loading_verb = "Connecting";
+  let loading_action = "Connect";
+  let loading_error = "";
 
-  let setting_time = fetch(API_URL + "/set_time?" + getTimeParameter(), {"method": "POST"});
-  let loading_chartjs = import('chart.js');
-
-  let loading = Promise.all([setting_time, loading_chartjs]);
   let deferreds = [];
   let getting_stats = new Promise(function(resolve, reject){
     deferreds.push({resolve: resolve, reject: reject});
@@ -22,41 +24,69 @@
 
   async function get_stats(){
     let response = await fetch(API_URL + '/stats');
+    console.log(response.status);
     let stats = await response.json();
     deferreds[0].resolve(stats);
   }
 
   async function main(){
-    await loading;
-    get_stats();
+    try {
+      var response = await fetch(API_URL + "/set_time?" + getTimeParameter(), {"method": "POST"});
+    } catch(e){
+      loading_error = e;
+      return;
+    }
+    if (response.status != 200){
+      loading_error = "Couldn't set the time.";
+      return;
+    }
+
+    loading_verb = "Loading";
+    loading_action = "Load";
+    try {
+      var response = await import('chart.js');
+    } catch(e){
+      loading_error = e;
+      return;
+    }
+    loading = false;
+    await get_stats();
   }
 
   main();
 </script>
 
+<style>
+  .progress-bar {
+    min-width: 300px;
+  }
+</style>
+
 <main>
-{#await loading}
-  <p>loading...</p>
-{:then }
+
+{#if loading}
+  <FullPageProgress action={loading_action} action_verb={loading_verb} error={loading_error}/>
+{:else}
   <h1>Power Meter</h1>
-    <div class="row">
-        <div class="col m6">
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell--span-6">
             <h2>Realtime</h2>
             <canvas id="realtime"/>
         </div>
-        <div class="col m6">
+        <div class="mdc-layout-grid__cell--span-6">
             <h2>Today's Power Usage</h2>
             <canvas id="today_power"/>
         </div>
+      </div>
     </div>
-    <div class="row">
-        <div class="col m6">
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class="mdc-layout-grid__cell--span-6">
           <DiskUsage getting_stats={getting_stats}/>
         </div>
+      </div>
     </div>
-{:catch error}
-  <p style="color: red">Uhh Ohhh. Something went wrong:</p>
-  <p style="color: red">{error.message}</p>
-{/await}
+{/if}
 
 </main>
